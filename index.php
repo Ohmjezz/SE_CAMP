cd1d31b080c98e092bea5f5812a95f9dac6e2f69
ohmjess <130266099+Ohmjezz@users.noreply.github.com>
2023-12-19 22:51:22 +0700

    test

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
 </head>
+
 <body>
-    <h1>This is a Multiplication table of 8</h1>
-    <?php 
-        $num = 8;
-        for ($i=1; $i <= 12; $i++) { 
-            echo $num . " x ". $i ." = ".$num * $i . "<br>" . "---------------<br>";
-        }         
-    ?>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    <div class="box">
+        <table>
+            <tbody>
+                <?php
+                for ($i=1; $i <= 24; $i++) { 
+                    $result = $multi_x * $i;
+                    echo "<tr>
+                        <td> $multi_x </td>
+                        <td>x</td>
+                        <td> $i </td>
+                        <td>=</td>
+                        <td> $result </td>
+                        </tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <style>
+        .box { 
+            width: 150px;
+            height: 275px;
+            overflow: scroll;
+        }
+    </style>
 </body>
+
 </html>
